8db83cc799893498512bce9728988e1890781d51
Sheraz Ahmed <sherazahmed93@gmail.com>
2022-08-14 02:51:15 +0500

    Connected with Ganache Wallet

--- routes/contracts.js
+++ routes/contracts.js
@@ -5,14 +5,20 @@ const ganache = require("ganache");
 
 /* GET users listing. */
 router.get("/wallets", async (req, res, next) => {
-  const options = {address: 'http://127.0.0.1:7545'};
-  const provider = ganache.provider(options);
-  const accounts = await provider.request({
-    method: "eth_accounts",
-    params: [],
-  });
+  const rpcURL = "http://127.0.0.1:7545";
+  var web3 = new Web3(rpcURL);
 
-  res.json({ accounts });
+  accounts = await Promise.all(
+    (
+      await web3.eth.personal.getAccounts()
+    ).map(async (wallet) => ({
+      wallet,
+      balance: `${web3.utils.fromWei(
+        await web3.eth.getBalance(wallet)
+      )} ethers`,
+    }))
+  );
+  res.json(accounts);
 });
 
 module.exports = router;
