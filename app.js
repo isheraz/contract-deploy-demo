a2c3cc1635bec63cf98a1623e8031c05f056acdc
Sheraz Ahmed <sherazahmed93@gmail.com>
2022-08-13 15:05:14 +0500

    Exploring Query Params

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send("{message: 'Hello World!'}")
+})
+app.get('/understand-object', (req, res) => {
+    console.table(req.query);
+    const reply = {message: [{id: '1'}, {id: 2}, {id: 3}], class: "blockchain-js-101", test: {
+        message: "Message of Test. Accessable by obj.test.message",
+        request: req.headers
+    }}
+    res.json(reply);
 })
 
 app.listen(port, () => {
